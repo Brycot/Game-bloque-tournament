18b3c2d5e1572b31c9c32b68c273b9f428dd6631
Brycot <bryalquiroz@gmail.com>
2025-05-02 15:32:08 -0500

    FEATURE: Refactor App component to utilize Layout, LeaderboardSection, MarketSection, and LegendSection, enhancing the structure and presentation of the user interface.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Layout from './components/layout/Layout';
+import LeaderboardSection from './components/sections/LeaderboardSection';
+import MarketSection from './components/sections/MarketSection';
+import LegendSection from './components/sections/LegendSection';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Layout>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-5 gap-8">
+          <div className="lg:col-span-3 lg:order-1 flex flex-col gap-8">
+            <LeaderboardSection />
+            <LegendSection />
+          </div>
+          <MarketSection />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Layout>
   )
 }
 
