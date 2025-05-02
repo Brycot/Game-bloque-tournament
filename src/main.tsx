f5712d6354757aab0a891a0f917e432020ffe495
Brycot <bryalquiroz@gmail.com>
2025-05-02 15:31:03 -0500

    FEATURE: Integrate SWR for data fetching in the main application component, enhancing data management with configuration for revalidation and refresh intervals.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SWRConfig } from 'swr'
 import './index.css'
 import App from './App.tsx'
 
+const fetcher = (url: string) => fetch(url).then(res => res.json())
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SWRConfig value={{
+      fetcher,
+      refreshInterval: 50000,
+      revalidateOnFocus: true,
+      revalidateOnReconnect: true,
+    }}>
     <App />
+    </SWRConfig>
   </StrictMode>,
 )
